d342a8443934de8f8efed68b86656efa080957b0
Satyam <Satyamdubey3626@gmail.com>
2026-02-19 10:30:23 -0800

    function parameters arguments

---
+++ Day_8(functions)/Task2.py
@@ -0,0 +1,19 @@
+#function with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"what it is like in {location}")
+greet_with("Satyam", "Bhopal")
+greet_with("MEE", "MumBai")
+
+# Positional arguments
+# def my_function(a,b,c):
+    #Do this with a
+#     do this with b
+#     do this with c
+# my_function(1,2,3)
+
+# for escaping with positional argument problem we could use "Keyword Arguments":
+
+# my_function(a=1, b=2, c=3) key word argument here order doen't matter
+# in previous code we used positional argunment instead of using positionl argument use keyword argument
+
